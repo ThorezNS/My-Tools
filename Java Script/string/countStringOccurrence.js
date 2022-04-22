6c0e24e9e9d05a3645ad2a38d060836ed18f0323
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-04-22 21:47:26 +0200

    example

--- Java Script/string/countStringOccurrence.js
+++ Java Script/string/countStringOccurrence.js
@@ -2,4 +2,7 @@ export const countStringOccurrence = (searchString, string) => {
   return string.split(searchString).length - 1;
 };
 
+countStringOccurrence('cat', 'atcat cat at that cccat'); //3
+countStringOccurrence('c', 'atcat cat at that cccat'); //5
+
 //#2 Podany string zostaje przekonwertowany na tablicę, która zawiera elementy (stringi) oddzielone szukanym stringiem. Elementów jest tyle ile razy występuje szukany string, plus jeden na który składa się początkowy element. Dlatego przy odczytywaniu ilości elementów, przy pomocy własności length, na końcu odejmowany jest jeden
